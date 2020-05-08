158220d22a15c3e49904f1b0767bb47b6a4add7f
bradtaniguchi <bradtaniguchi@gmail.com>
2020-05-07 22:23:56 -0700

    Add jest test support

---
+++ src/app/components/calculator/calculator.service.spec.ts
@@ -0,0 +1,9 @@
+import { CalculatorService } from './calculator.service';
+
+describe('CalculatorService', () => {
+  const service = new CalculatorService();
+  test.todo('this is a todo');
+  // this will be replaced by actual tests later
+  test('true is true', () => expect(true).toBeTruthy());
+  test('service exists', () => expect(service).toBeTruthy());
+});
