cf0c0dbd7bcde6183a28d6587da79f48c6d26887
brad taniguchi <bradtaniguchi@gmail.com>
2020-05-10 22:28:37 -0700

    Refactor to page object

--- e2e/src/integration/calculator.spec.ts
+++ e2e/src/integration/calculator.spec.ts
@@ -1,122 +1,102 @@
+import {
+  CalculatorPageObject,
+  CalculatorButtons
+} from '../fixtures/calculator.po';
+
 describe('calculator', () => {
+  const calculatorPageObject = new CalculatorPageObject();
   before(() => {
     cy.visit('');
   });
   afterEach(() => {
     cy.reload();
   });
-  // copy of the button definitions
-  // used to display in the calculator.
-  const mainButtons = [
-    {
-      value: 7,
-      word: 'seven'
-    },
-    {
-      value: 8,
-      word: 'eight'
-    },
-    {
-      value: 9,
-      word: 'nine'
-    },
-    {
-      value: 4,
-      word: 'four'
-    },
-    {
-      value: 5,
-      word: 'five'
-    },
-    {
-      value: 6,
-      word: 'six'
-    },
-    {
-      value: 1,
-      word: 'one'
-    },
-    {
-      value: 2,
-      word: 'two'
-    },
-    {
-      value: 3,
-      word: 'three'
-    },
-    {
-      value: 'C',
-      word: 'clear'
-    },
-    {
-      value: 0,
-      word: 'zero'
-    },
-    {
-      value: '.',
-      word: 'decimal'
-    }
-  ];
-  const actionButtons = [
-    'add',
-    'subtract',
-    'multiply',
-    'divide',
-    'decimal',
-    'clear',
-    'equals'
-  ];
   describe('element exist', () => {
-    it('false test', () => {
-      expect(cy.get('#nope').should('not.exist'));
-    });
-
     // make sure every single one exists
     it('main buttons', () => {
-      mainButtons.forEach((mainButton) => {
-        expect(cy.get(`#${mainButton.word}`)).exist;
-      });
+      calculatorPageObject.mainButtons.forEach(
+        (mainButton) => expect(cy.get(`#${mainButton}`)).exist
+      );
     });
     it('action buttons', () => {
-      actionButtons.forEach((id) => {
-        expect(cy.get(`#${id}`)).exist;
-      });
+      calculatorPageObject.actionButtons.forEach(
+        (id) => expect(cy.get(`#${id}`)).exist
+      );
     });
   });
   it('clear does nothing initially', () => {
-    cy.get('#display').should('be.visible').should('have.text', ' 0 ');
-    cy.get('#equals').should('be.visible').click();
-    cy.get('#display').should('be.visible').should('have.text', ' 0 ');
+    calculatorPageObject
+      .getDisplay()
+      .should('be.visible')
+      .should('have.text', ' 0 ');
+    calculatorPageObject
+      .getButton(CalculatorButtons.equals)
+      .should('be.visible')
+      .click();
+    calculatorPageObject
+      .getDisplay()
+      .should('be.visible')
+      .should('have.text', ' 0 ');
   });
   it('1+1 equals 2', () => {
-    cy.get('#display').should('be.visible').should('have.text', ' 0 ');
+    calculatorPageObject
+      .getDisplay()
+      .should('be.visible')
+      .should('have.text', ' 0 ');
 
-    cy.get('#one').should('be.visible').click();
-    cy.get('#display').should('have.text', ' 1 ');
+    calculatorPageObject
+      .getButton(CalculatorButtons.one)
+      .should('be.visible')
+      .click();
+    calculatorPageObject.getDisplay().should('have.text', ' 1 ');
 
-    cy.get('#add').should('be.visible').click();
-    cy.get('#display').should('have.text', ' 1+ ');
+    calculatorPageObject
+      .getButton(CalculatorButtons.add)
+      .should('be.visible')
+      .click();
+    calculatorPageObject.getDisplay().should('have.text', ' 1+ ');
 
-    cy.get('#one').should('be.visible').click();
-    cy.get('#display').should('have.text', ' 1+1 ');
+    calculatorPageObject
+      .getButton(CalculatorButtons.one)
+      .should('be.visible')
+      .click();
+    calculatorPageObject.getDisplay().should('have.text', ' 1+1 ');
 
-    cy.get('#equals').should('be.visible').click();
-    cy.get('#display').should('have.text', ' 2 ');
+    calculatorPageObject
+      .getButton(CalculatorButtons.equals)
+      .should('be.visible')
+      .click();
+    calculatorPageObject.getDisplay().should('have.text', ' 2 ');
   });
   it('clear clears entered values (2/2)', () => {
-    cy.get('#display').should('be.visible').should('have.text', ' 0 ');
+    calculatorPageObject
+      .getDisplay()
+      .should('be.visible')
+      .should('have.text', ' 0 ');
 
-    cy.get('#two').should('be.visible').click();
-    cy.get('#display').should('have.text', ' 2 ');
+    calculatorPageObject
+      .getButton(CalculatorButtons.two)
+      .should('be.visible')
+      .click();
+    calculatorPageObject.getDisplay().should('have.text', ' 2 ');
 
-    cy.get('#divide').should('be.visible').click();
-    cy.get('#display').should('have.text', ' 2/ ');
+    calculatorPageObject
+      .getButton(CalculatorButtons.divide)
+      .should('be.visible')
+      .click();
+    calculatorPageObject.getDisplay().should('have.text', ' 2/ ');
 
-    cy.get('#two').should('be.visible').click();
-    cy.get('#display').should('have.text', ' 2/2 ');
+    calculatorPageObject
+      .getButton(CalculatorButtons.two)
+      .should('be.visible')
+      .click();
+    calculatorPageObject.getDisplay().should('have.text', ' 2/2 ');
 
-    cy.get('#clear').should('be.visible').click();
-    cy.get('#display').should('have.text', ' 0 ');
+    calculatorPageObject
+      .getButton(CalculatorButtons.clear)
+      .should('be.visible')
+      .click();
+    calculatorPageObject.getDisplay().should('have.text', ' 0 ');
   });
   it('dividing by zero is recoverable');
   it('uses previous result after = ');
